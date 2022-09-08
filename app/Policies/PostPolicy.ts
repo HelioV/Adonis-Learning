036da73ece1431a1ba4cd877fc5e5d5b4c151251
Hélio Vicente <jonquerfutila@gmail.com>
2022-09-08 16:57:16 +0100

    perf Ajustado as politicas e permissões

---
+++ app/Policies/PostPolicy.ts
@@ -0,0 +1,13 @@
+import { BasePolicy } from '@ioc:Adonis/Addons/Bouncer'
+import User from 'App/Models/User'
+import Post from 'App/Models/Post'
+
+export default class PostPolicy extends BasePolicy {
+	public async viewList(user: User) {}
+	public async view(user: User, post: Post) {
+		return user.id===post.id;
+	}
+	public async create(user: User) {}
+	public async update(user: User, post: Post) {}
+	public async delete(user: User, post: Post) {}
+}
--- start/include/posts/post.ts
+++ start/include/posts/post.ts
@@ -1,3 +1,11 @@
 import Route from '@ioc:Adonis/Core/Route'
-
+import Post from 'App/Models/Post'
+import authConfig from 'Config/auth'
 Route.resource('posts','PostsController').apiOnly()
+ 
+Route.get('post/:id', async ({ bouncer, request }) => {
+    const post = await Post.findOrFail(request.param('id'))
+    await bouncer
+    .with('PostPolicy')
+    .authorize('view', post)
+  }).middleware('auth')
